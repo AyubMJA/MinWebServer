a752314a934f0ff3f6760e3e00bbf11fcc070664
Ayub Ali <ayubali8448@icloud.com>
2021-09-23 10:09:59 -0400

    Add project files.

---
+++ MinWebServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+namespace MinWebServer
+{
+    class App
+    {
+        static void Main()
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(context => context.Response.WriteAsync("<h1>Min web server Hello World !</h1>")))
+                .Build()
+                .Run();
+        }
+    }
+}
